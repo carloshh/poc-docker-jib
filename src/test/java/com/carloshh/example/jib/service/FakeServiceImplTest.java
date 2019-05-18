6b834e70010abac17d7c557f51c56dacf6007cf8
Carlos Hernández <carloshh@me.com>
2019-05-18 23:17:46 +0200

    Uncovered lines, quality gate fail

---
+++ src/test/java/com/carloshh/example/jib/service/FakeServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.carloshh.example.jib.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FakeServiceImplTest {
+
+    @Test
+    public void uncoveredLines() {
+    }
+}
